7e018e173c38897e90c52f564f5d889becec37f5
Saurav <saurav.dutta72@yahoo.com>
2022-12-21 10:22:37 +0530

    added middlewares

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
+
+app.use(logger)
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, '/public'))) //Tells express where to look for static files
 
@@ -27,5 +40,7 @@ app.all('*', (req, res) => {
 
 mongoose.set('strictQuery', false)
 mongoose.connect('mongodb+srv://CsMessagingApp:cs123messaging123app123@cluster0.knsc5up.mongodb.net/?retryWrites=true&w=majority')
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
